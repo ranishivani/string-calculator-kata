97983ae7574879c10fea90c2104dba67ba8ec23c
Shivani Rani <shivanirani816@gmail.com>
2025-06-10 00:58:01 +0530

    Changes to handle new line and custom delimiter

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -11,3 +11,11 @@ test('returns 1 for "1"', () => {
 test('returns 3 for "1,2"', () => {
   expect(add("1,2")).toBe(3);
 });
+
+test("handles new lines between numbers", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
+
+test("supports custom delimiter specified like //;\n", () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,19 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
-  return numbers.split(",").reduce((sum, num) => sum + Number(num), 0);
+  let delimiter = /,|\n/;
+  let nums = numbers;
+
+  // Custom delimiter syntax
+  if (numbers.startsWith("//")) {
+    const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
+    if (delimiterMatch) {
+      delimiter = new RegExp(delimiterMatch[1]);
+      nums = numbers.split("\n")[1];
+    }
+  }
+
+  return nums.split(delimiter).reduce((sum, num) => sum + Number(num), 0);
 }
 
 module.exports = add;
