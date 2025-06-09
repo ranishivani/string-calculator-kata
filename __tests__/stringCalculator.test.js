ba5eb8ab1fdb25d6fa49099e92b6d20da1490c68
Shivani Rani <shivanirani816@gmail.com>
2025-06-10 00:59:45 +0530

    Changes to handle negative numbers and number greater than 1000

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -19,3 +19,11 @@ test("handles new lines between numbers", () => {
 test("supports custom delimiter specified like //;\n", () => {
   expect(add("//;\n1;2")).toBe(3);
 });
+
+test("throws error for negative numbers", () => {
+  expect(() => add("1,-2,-3")).toThrow("Negatives not allowed: -2,-3");
+});
+
+test('ignores numbers greater than 1000', () => {
+  expect(add("2,1001")).toBe(2);
+});
--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -4,7 +4,6 @@ function add(numbers) {
   let delimiter = /,|\n/;
   let nums = numbers;
 
-  // Custom delimiter syntax
   if (numbers.startsWith("//")) {
     const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
     if (delimiterMatch) {
@@ -13,7 +12,13 @@ function add(numbers) {
     }
   }
 
-  return nums.split(delimiter).reduce((sum, num) => sum + Number(num), 0);
+  const parts = nums.split(delimiter).map(Number);
+  const negatives = parts.filter((n) => n < 0);
+  if (negatives.length > 0) {
+    throw new Error(`Negatives not allowed: ${negatives.join(",")}`);
+  }
+
+  return parts.filter((n) => n <= 1000).reduce((sum, n) => sum + n, 0);
 }
 
 module.exports = add;
